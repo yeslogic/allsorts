9395601da424c7acc6c4feb8a13b9c19f96a6c6e
Brendan Zabarauskas <bjzaba@yahoo.com.au>
2017-09-08 15:33:03 +1000

    Add more possibilities for configuring cbindgen

--- allsorts-capi/README.md
+++ allsorts-capi/README.md
@@ -2,6 +2,19 @@
 
 A C API for the Allsorts font shaping engine.
 
+# Header generation
+
+We automatically generate C headers using the [cbindgen](https://github.com/eqrion/cbindgen).
+In lieu of better options for distributing these headers, the following environment variables
+can be configured to alter cbindgen's behaviour:
+
+- `ALLSORTS_CBINDGEN_OUT_DIR`: The output directory in which to generate `allsorts.h`
+- `ALLSORTS_CBINDGEN_CONFIG_FILE`: The filename of the TOML file used to configure cbindgen
+
+Note that these variables are _relative to the `allsorts-capi` source directory_, which is
+usually not what you want. You will have to supply a full path in order to generate it
+anywhere else.
+
 ## License
 
 The Allsorts C API is distributed under the terms of the Apache License (Version 2.0).
--- allsorts-capi/build.rs
+++ allsorts-capi/build.rs
@@ -1,15 +1,22 @@
 extern crate cbindgen;
 
+use cbindgen::Config;
 use std::env;
 
 fn main() {
     let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
     let header_out_dir =
-        env::var("ALLSORTS_HEADER_OUT_DIR").unwrap_or_else(|_| "include".to_owned());
+        env::var("ALLSORTS_CBINDGEN_OUT_DIR").unwrap_or_else(|_| "include".to_owned());
 
     println!("cargo:rerun-if-changed=src/lib.rs");
-    cbindgen::generate(&crate_dir).unwrap().write_to_file(
-        &(header_out_dir +
-              "/allsorts.h"),
-    );
+
+    let bindings = match env::var("ALLSORTS_CBINDGEN_CONFIG_FILE") {
+        Err(_) => cbindgen::generate(&crate_dir).unwrap(),
+        Ok(path) => {
+            let config = Config::from_file(&path).unwrap();
+            cbindgen::generate_with_config(&crate_dir, config).unwrap()
+        }
+    };
+
+    bindings.write_to_file(&(header_out_dir + "/allsorts.h"));
 }
