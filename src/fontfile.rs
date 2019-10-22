45c34bc2a886bf0eeb6e9d5cc9c9f2b8f22a48d2
Wesley Moore <wes@wezm.net>
2019-10-22 13:46:21 +1100

    Add wrapper for FontTableProvider trait object
    So that it can be used in places that want impl FontTableProvider.

--- src/fontfile.rs
+++ src/fontfile.rs
@@ -1,3 +1,5 @@
+use std::borrow::Cow;
+
 use crate::binary::read::{ReadBinary, ReadCtxt};
 use crate::error::{ParseError, ReadWriteError};
 use crate::tables::{FontTableProvider, OpenTypeFile, CFF_MAGIC, TTCF_MAGIC, TTF_MAGIC};
@@ -8,6 +10,10 @@ pub enum FontFile<'a> {
     OpenType(OpenTypeFile<'a>),
     Woff(WoffFile<'a>),
     Woff2(Woff2File<'a>),
+}
+
+pub struct FileTableProvider<'a> {
+    provider: Box<dyn FontTableProvider + 'a>,
 }
 
 impl<'a> ReadBinary<'a> for FontFile<'a> {
@@ -26,23 +32,36 @@ impl<'a> ReadBinary<'a> for FontFile<'a> {
     }
 }
 
+impl<'a> FontTableProvider for FileTableProvider<'a> {
+    fn table_data<'b>(&'b self, tag: u32) -> Result<Option<Cow<'b, [u8]>>, ParseError> {
+        self.provider.table_data(tag)
+    }
+
+    fn has_table(&self, tag: u32) -> bool {
+        self.provider.has_table(tag)
+    }
+}
+
 impl<'a> FontFile<'a> {
-    pub fn table_provider(
-        &'a self,
-        index: usize,
-    ) -> Result<Box<dyn FontTableProvider + 'a>, ReadWriteError> {
+    pub fn table_provider(&'a self, index: usize) -> Result<FileTableProvider<'a>, ReadWriteError> {
         match self {
             FontFile::OpenType(file) => {
                 let provider = file.font_provider(index)?;
-                Ok(Box::new(provider))
+                Ok(FileTableProvider {
+                    provider: Box::new(provider),
+                })
             }
             FontFile::Woff(file) => {
                 // This clone is relatively cheap as WoffFile is mostly holding borrowed data
-                Ok(Box::new(file.clone()))
+                Ok(FileTableProvider {
+                    provider: Box::new(file.clone()),
+                })
             }
             FontFile::Woff2(file) => {
                 let provider = file.table_provider(index)?;
-                Ok(Box::new(provider))
+                Ok(FileTableProvider {
+                    provider: Box::new(provider),
+                })
             }
         }
     }
