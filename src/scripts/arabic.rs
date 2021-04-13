9b9aa3409c4e3ada9d175ac0a8e829f66e6549bc
Adrian Wong <adrianwjw@gmail.com>
2021-04-13 16:57:56 +1000

    arabic: Use enum to define canonical combining class

--- src/scripts/arabic.rs
+++ src/scripts/arabic.rs
@@ -14,7 +14,7 @@ use unicode_joining_type::{get_joining_type, JoiningType};
 #[derive(Clone)]
 struct ArabicData {
     joining_type: JoiningType,
-    canonical_combining_class: u8,
+    canonical_combining_class: CanonicalCombiningClass,
     feature_tag: u32,
 }
 
@@ -45,7 +45,7 @@ impl ArabicGlyph {
             || self.extra_data.joining_type == JoiningType::JoinCausing
     }
 
-    fn canonical_combining_class(&self) -> u8 {
+    fn canonical_combining_class(&self) -> CanonicalCombiningClass {
         self.extra_data.canonical_combining_class
     }
 
@@ -70,7 +70,7 @@ impl From<&RawGlyph<()>> for ArabicGlyph {
 
         let canonical_combining_class = match raw_glyph.glyph_origin {
             GlyphOrigin::Char(c) => canonical_combining_class(c),
-            GlyphOrigin::Direct => 0,
+            GlyphOrigin::Direct => CanonicalCombiningClass::CCC000,
         };
 
         ArabicGlyph {
@@ -266,11 +266,12 @@ fn apply_lookups(
 
 /// Reorder Arabic marks per AMTRA. See: https://www.unicode.org/reports/tr53/.
 fn reorder_marks(glyphs: &mut Vec<ArabicGlyph>) {
-    for gs in glyphs.split_mut(|g| g.canonical_combining_class() == 0) {
+    for gs in glyphs.split_mut(|g| g.canonical_combining_class() == CanonicalCombiningClass::CCC000)
+    {
         reorder_marks_nfd(gs);
         reorder_marks_shadda(gs);
-        reorder_marks_other_combining(gs, 230);
-        reorder_marks_other_combining(gs, 220);
+        reorder_marks_other_combining(gs, CanonicalCombiningClass::CCC230);
+        reorder_marks_other_combining(gs, CanonicalCombiningClass::CCC220);
     }
 }
 
@@ -289,7 +290,7 @@ fn reorder_marks_shadda(glyphs: &mut [ArabicGlyph]) {
     // 2a. Move any Shadda characters to the beginning of S, where S is a max
     // length substring of non-starter characters.
     fn comparator(g1: &ArabicGlyph, _g2: &ArabicGlyph) -> Ordering {
-        if g1.canonical_combining_class() == 33 {
+        if g1.canonical_combining_class() == CanonicalCombiningClass::CCC033 {
             Ordering::Less
         } else {
             Ordering::Equal
@@ -298,8 +299,8 @@ fn reorder_marks_shadda(glyphs: &mut [ArabicGlyph]) {
     glyphs.sort_by(comparator)
 }
 
-fn reorder_marks_other_combining(glyphs: &mut [ArabicGlyph], ccc: u8) {
-    assert!(ccc == 220 || ccc == 230);
+fn reorder_marks_other_combining(glyphs: &mut [ArabicGlyph], ccc: CanonicalCombiningClass) {
+    assert!(ccc == CanonicalCombiningClass::CCC220 || ccc == CanonicalCombiningClass::CCC230);
 
     // Get the start index of a possible sequence of characters with canonical
     // combining class equal to `ccc`. (Assumes that `glyphs` is normalised to
@@ -341,105 +342,123 @@ fn is_modifier_combining_mark(ch: char) -> bool {
     }
 }
 
-fn canonical_combining_class(ch: char) -> u8 {
+#[derive(Copy, Clone, PartialEq, PartialOrd, Eq, Ord)]
+enum CanonicalCombiningClass {
+    CCC000,
+    CCC027,
+    CCC028,
+    CCC029,
+    CCC030,
+    CCC031,
+    CCC032,
+    CCC033,
+    CCC034,
+    CCC035,
+    CCC220,
+    CCC230,
+}
+
+fn canonical_combining_class(ch: char) -> CanonicalCombiningClass {
+    use CanonicalCombiningClass::*;
+
     match ch {
-        '\u{064B}' => 27,  // Fathatan
-        '\u{08F0}' => 27,  // Open Fathatan
-        '\u{064C}' => 28,  // Dammatan
-        '\u{08F1}' => 28,  // Open Dammatan
-        '\u{064D}' => 29,  // Kasratan
-        '\u{08F2}' => 29,  // Open Kasratan
-        '\u{0618}' => 30,  // Small Fatha
-        '\u{064E}' => 30,  // Fatha
-        '\u{0619}' => 31,  // Small Damma
-        '\u{064F}' => 31,  // Damma
-        '\u{061A}' => 32,  // Small Kasra
-        '\u{0650}' => 32,  // Kasra
-        '\u{0651}' => 33,  // Shadda
-        '\u{0652}' => 34,  // Sukun
-        '\u{0670}' => 35,  // Letter Superscript Alef
-        '\u{0655}' => 220, // Hamza Below
-        '\u{0656}' => 220, // Subscript Alef
-        '\u{065C}' => 220, // Vowel Sign Dot Below
-        '\u{065F}' => 220, // Wavy Hamza Below
-        '\u{06E3}' => 220, // Small Low Seen
-        '\u{06EA}' => 220, // Empty Centre Low Stop
-        '\u{06ED}' => 220, // Small Low Meem
-        '\u{08D3}' => 220, // Small Low Waw
-        '\u{08E3}' => 220, // Turned Damma Below
-        '\u{08E6}' => 220, // Curly Kasra
-        '\u{08E9}' => 220, // Curly Kasratan
-        '\u{08ED}' => 220, // Tone One Dot Below
-        '\u{08EE}' => 220, // Tone Two Dots Below
-        '\u{08EF}' => 220, // Tone Loop Below
-        '\u{08F6}' => 220, // Kasra With Dot Below
-        '\u{08F9}' => 220, // Left Arrowhead Below
-        '\u{08FA}' => 220, // Right Arrowhead Below
-        '\u{0610}' => 230, // Sign Sallallahou Alayhe Wassallam
-        '\u{0611}' => 230, // Sign Alayhe Assallam
-        '\u{0612}' => 230, // Sign Rahmatullah Alayhe
-        '\u{0613}' => 230, // Sign Radi Allahou Anhu
-        '\u{0614}' => 230, // Sign Takhallus
-        '\u{0615}' => 230, // Small High Tah
-        '\u{0616}' => 230, // Small High Ligature Alef With Lam With Yeh
-        '\u{0617}' => 230, // Small High Zain
-        '\u{0653}' => 230, // Maddah Above
-        '\u{0654}' => 230, // Hamza Above
-        '\u{0657}' => 230, // Inverted Damma
-        '\u{0658}' => 230, // Mark Noon Ghunna
-        '\u{0659}' => 230, // Zwarakay
-        '\u{065A}' => 230, // Vowel Sign Small V Above
-        '\u{065B}' => 230, // Vowel Sign Inverted Small V Above
-        '\u{065D}' => 230, // Reversed Damma
-        '\u{065E}' => 230, // Fatha With Two Dots
-        '\u{06D6}' => 230, // Small High Ligature Sad With Lam With Alef Maksura
-        '\u{06D7}' => 230, // Small High Ligature Qaf With Lam With Alef Maksura
-        '\u{06D8}' => 230, // Small High Meem Initial Form
-        '\u{06D9}' => 230, // Small High Lam Alef
-        '\u{06DA}' => 230, // Small High Jeem
-        '\u{06DB}' => 230, // Small High Three Dots
-        '\u{06DC}' => 230, // Small High Seen
-        '\u{06DF}' => 230, // Small High Rounded Zero
-        '\u{06E0}' => 230, // Small High Upright Rectangular Zero
-        '\u{06E1}' => 230, // Small High Dotless Head Of Khah
-        '\u{06E2}' => 230, // Small High Meem Isolated Form
-        '\u{06E4}' => 230, // Small High Madda
-        '\u{06E7}' => 230, // Small High Yeh
-        '\u{06E8}' => 230, // Small High Noon
-        '\u{06EB}' => 230, // Empty Centre High Stop
-        '\u{06EC}' => 230, // Rounded High Stop With Filled Centre
-        '\u{08D4}' => 230, // Small High Word Ar-Rub
-        '\u{08D5}' => 230, // Small High Sad
-        '\u{08D6}' => 230, // Small High Ain
-        '\u{08D7}' => 230, // Small High Qaf
-        '\u{08D8}' => 230, // Small High Noon With Kasra
-        '\u{08D9}' => 230, // Small Low Noon With Kasra
-        '\u{08DA}' => 230, // Small High Word Ath-Thalatha
-        '\u{08DB}' => 230, // Small High Word As-Sajda
-        '\u{08DC}' => 230, // Small High Word An-Nisf
-        '\u{08DD}' => 230, // Small High Word Sakta
-        '\u{08DE}' => 230, // Small High Word Qif
-        '\u{08DF}' => 230, // Small High Word Waqfa
-        '\u{08E0}' => 230, // Small High Footnote Marker
-        '\u{08E1}' => 230, // Small High Sign Safha
-        '\u{08E4}' => 230, // Curly Fatha
-        '\u{08E5}' => 230, // Curly Damma
-        '\u{08E7}' => 230, // Curly Fathatan
-        '\u{08E8}' => 230, // Curly Dammatan
-        '\u{08EA}' => 230, // Tone One Dot Above
-        '\u{08EB}' => 230, // Tone Two Dots Above
-        '\u{08EC}' => 230, // Tone Loop Above
-        '\u{08F3}' => 230, // Small High Waw
-        '\u{08F4}' => 230, // Fatha With Ring
-        '\u{08F5}' => 230, // Fatha With Dot Above
-        '\u{08F7}' => 230, // Left Arrowhead Above
-        '\u{08F8}' => 230, // Right Arrowhead Above
-        '\u{08FB}' => 230, // Double Right Arrowhead Above
-        '\u{08FC}' => 230, // Double Right Arrowhead Above With Dot
-        '\u{08FD}' => 230, // Right Arrowhead Above With Dot
-        '\u{08FE}' => 230, // Damma With Dot
-        '\u{08FF}' => 230, // Mark Sideways Noon Ghunna
-        _ => 0,
+        '\u{064B}' => CCC027, // Fathatan
+        '\u{08F0}' => CCC027, // Open Fathatan
+        '\u{064C}' => CCC028, // Dammatan
+        '\u{08F1}' => CCC028, // Open Dammatan
+        '\u{064D}' => CCC029, // Kasratan
+        '\u{08F2}' => CCC029, // Open Kasratan
+        '\u{0618}' => CCC030, // Small Fatha
+        '\u{064E}' => CCC030, // Fatha
+        '\u{0619}' => CCC031, // Small Damma
+        '\u{064F}' => CCC031, // Damma
+        '\u{061A}' => CCC032, // Small Kasra
+        '\u{0650}' => CCC032, // Kasra
+        '\u{0651}' => CCC033, // Shadda
+        '\u{0652}' => CCC034, // Sukun
+        '\u{0670}' => CCC035, // Letter Superscript Alef
+        '\u{0655}' => CCC220, // Hamza Below
+        '\u{0656}' => CCC220, // Subscript Alef
+        '\u{065C}' => CCC220, // Vowel Sign Dot Below
+        '\u{065F}' => CCC220, // Wavy Hamza Below
+        '\u{06E3}' => CCC220, // Small Low Seen
+        '\u{06EA}' => CCC220, // Empty Centre Low Stop
+        '\u{06ED}' => CCC220, // Small Low Meem
+        '\u{08D3}' => CCC220, // Small Low Waw
+        '\u{08E3}' => CCC220, // Turned Damma Below
+        '\u{08E6}' => CCC220, // Curly Kasra
+        '\u{08E9}' => CCC220, // Curly Kasratan
+        '\u{08ED}' => CCC220, // Tone One Dot Below
+        '\u{08EE}' => CCC220, // Tone Two Dots Below
+        '\u{08EF}' => CCC220, // Tone Loop Below
+        '\u{08F6}' => CCC220, // Kasra With Dot Below
+        '\u{08F9}' => CCC220, // Left Arrowhead Below
+        '\u{08FA}' => CCC220, // Right Arrowhead Below
+        '\u{0610}' => CCC230, // Sign Sallallahou Alayhe Wassallam
+        '\u{0611}' => CCC230, // Sign Alayhe Assallam
+        '\u{0612}' => CCC230, // Sign Rahmatullah Alayhe
+        '\u{0613}' => CCC230, // Sign Radi Allahou Anhu
+        '\u{0614}' => CCC230, // Sign Takhallus
+        '\u{0615}' => CCC230, // Small High Tah
+        '\u{0616}' => CCC230, // Small High Ligature Alef With Lam With Yeh
+        '\u{0617}' => CCC230, // Small High Zain
+        '\u{0653}' => CCC230, // Maddah Above
+        '\u{0654}' => CCC230, // Hamza Above
+        '\u{0657}' => CCC230, // Inverted Damma
+        '\u{0658}' => CCC230, // Mark Noon Ghunna
+        '\u{0659}' => CCC230, // Zwarakay
+        '\u{065A}' => CCC230, // Vowel Sign Small V Above
+        '\u{065B}' => CCC230, // Vowel Sign Inverted Small V Above
+        '\u{065D}' => CCC230, // Reversed Damma
+        '\u{065E}' => CCC230, // Fatha With Two Dots
+        '\u{06D6}' => CCC230, // Small High Ligature Sad With Lam With Alef Maksura
+        '\u{06D7}' => CCC230, // Small High Ligature Qaf With Lam With Alef Maksura
+        '\u{06D8}' => CCC230, // Small High Meem Initial Form
+        '\u{06D9}' => CCC230, // Small High Lam Alef
+        '\u{06DA}' => CCC230, // Small High Jeem
+        '\u{06DB}' => CCC230, // Small High Three Dots
+        '\u{06DC}' => CCC230, // Small High Seen
+        '\u{06DF}' => CCC230, // Small High Rounded Zero
+        '\u{06E0}' => CCC230, // Small High Upright Rectangular Zero
+        '\u{06E1}' => CCC230, // Small High Dotless Head Of Khah
+        '\u{06E2}' => CCC230, // Small High Meem Isolated Form
+        '\u{06E4}' => CCC230, // Small High Madda
+        '\u{06E7}' => CCC230, // Small High Yeh
+        '\u{06E8}' => CCC230, // Small High Noon
+        '\u{06EB}' => CCC230, // Empty Centre High Stop
+        '\u{06EC}' => CCC230, // Rounded High Stop With Filled Centre
+        '\u{08D4}' => CCC230, // Small High Word Ar-Rub
+        '\u{08D5}' => CCC230, // Small High Sad
+        '\u{08D6}' => CCC230, // Small High Ain
+        '\u{08D7}' => CCC230, // Small High Qaf
+        '\u{08D8}' => CCC230, // Small High Noon With Kasra
+        '\u{08D9}' => CCC230, // Small Low Noon With Kasra
+        '\u{08DA}' => CCC230, // Small High Word Ath-Thalatha
+        '\u{08DB}' => CCC230, // Small High Word As-Sajda
+        '\u{08DC}' => CCC230, // Small High Word An-Nisf
+        '\u{08DD}' => CCC230, // Small High Word Sakta
+        '\u{08DE}' => CCC230, // Small High Word Qif
+        '\u{08DF}' => CCC230, // Small High Word Waqfa
+        '\u{08E0}' => CCC230, // Small High Footnote Marker
+        '\u{08E1}' => CCC230, // Small High Sign Safha
+        '\u{08E4}' => CCC230, // Curly Fatha
+        '\u{08E5}' => CCC230, // Curly Damma
+        '\u{08E7}' => CCC230, // Curly Fathatan
+        '\u{08E8}' => CCC230, // Curly Dammatan
+        '\u{08EA}' => CCC230, // Tone One Dot Above
+        '\u{08EB}' => CCC230, // Tone Two Dots Above
+        '\u{08EC}' => CCC230, // Tone Loop Above
+        '\u{08F3}' => CCC230, // Small High Waw
+        '\u{08F4}' => CCC230, // Fatha With Ring
+        '\u{08F5}' => CCC230, // Fatha With Dot Above
+        '\u{08F7}' => CCC230, // Left Arrowhead Above
+        '\u{08F8}' => CCC230, // Right Arrowhead Above
+        '\u{08FB}' => CCC230, // Double Right Arrowhead Above
+        '\u{08FC}' => CCC230, // Double Right Arrowhead Above With Dot
+        '\u{08FD}' => CCC230, // Right Arrowhead Above With Dot
+        '\u{08FE}' => CCC230, // Damma With Dot
+        '\u{08FF}' => CCC230, // Mark Sideways Noon Ghunna
+        _ => CCC000,
     }
 }
 
