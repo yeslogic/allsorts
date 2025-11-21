787183829bdf96e9fa107523c9b9ef3fbb727a90
Wesley Moore <wes@wezm.net>
2025-11-21 10:33:01 +1000

    Fix docs generation on docs.rs
    https://github.com/rust-lang/rust/pull/138907

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@
 #![doc(html_logo_url = "https://github.com/yeslogic/allsorts/raw/master/allsorts.svg?sanitize=1")]
 // Adds Cargo feature annotations to items in the rustdoc output. Nightly
 // only, so behind a feature gate for now (which is enabled on docs.rs).
-#![cfg_attr(docsrs, feature(doc_auto_cfg))]
+#![cfg_attr(docsrs, feature(doc_cfg))]
 
 //! # Font parser, shaping engine, and subsetter
 //!
